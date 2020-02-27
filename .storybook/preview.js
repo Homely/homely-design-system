f558cd8036b42b5798b6f570b14823bf035bd4f6
Morgan Jackson <mail@morganjackson.com.au>
2020-02-28 09:59:03 +1100

    Setup a11y addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 import { GlobalStyle } from '../src/shared/global';
 import themeDecorator from './theme-decorator';
 import pageInfoDecorator from './page-info-decorator';
 
+
 addDecorator(themeDecorator);
 addDecorator(pageInfoDecorator);
+addDecorator(withA11y);
 addDecorator(story => (
   <>
     <GlobalStyle />
