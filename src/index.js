0a6bb862d9d7be496bf0e26ddb47420e4a1bcae0
Morgan Jackson <mail@morganjackson.com.au>
2020-02-28 11:55:23 +1100

    Setup build & Auto

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as theme from './shared/theme';
+import * as global from './shared/global';
+import * as icons from './shared/icons';
+
+export { theme, global, icons };
+
+export * from './Avatar';
+export * from './Button';
+export * from './Icon';
